93844e25038c3b7256bae8fb44fd0e66eb128091
Caribla <caribla4@gmail.com>
2024-03-09 13:01:53 +0100

    Update: User entity => getUsers can have an email query param to get an user by email

--- src/entities/user/controller.js
+++ src/entities/user/controller.js
@@ -2,6 +2,25 @@ import User from "./User.js";
 
 export const getUsers = async (req, res) => {
     try {
+        const email = req.query.email;
+
+        if(email){
+            const user = await User.findOne({email: email}, '-password');
+
+            if(!user){
+                return res.status(404).json({
+                    success: false,
+                    message: "You cant find a user with that email"
+                })
+            }
+
+            return res.status(200).json({
+                success: true,
+                message: "User retrieved succesfully",
+                data: user
+            })
+        }
+
         const users = await User.find({}, '-password');
 
         if(!users){
