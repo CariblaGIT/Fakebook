14b24032ec2d9c6e98265fa5de1da0d2df2f4a1a
Caribla <caribla4@gmail.com>
2024-03-11 10:00:19 +0100

    Fix: Seeders => Assign static IDs to users

--- src/entities/post/seeder/seederDefaultPosts.js
+++ src/entities/post/seeder/seederDefaultPosts.js
@@ -2,15 +2,12 @@ import 'dotenv/config';
 import Post from "../Post.js";
 import mongoose from "mongoose";
 import { dbConnection } from "../../../db.js"
-import User from '../../user/User.js';
 
 export const seederDefaultPosts = async () => {
     try {
 		await dbConnection();
-        const user1 = await User.findOne({name: "user"});
-        const user1Id = (user1._id).toString()
-        const user2 = await User.findOne({name: "user2"});
-        const user2Id = (user2._id).toString()
+        const user1Id = "65ed7d2f6fa9305f1c42440f"
+        const user2Id = "65ed7d2f6fa9305f1c424410"
         const likes = [user2Id, user1Id];
 		await Post.create([
 			{
--- src/entities/user/seeder/seederDefaultUsers.js
+++ src/entities/user/seeder/seederDefaultUsers.js
@@ -12,25 +12,29 @@ export const seederDefaultUsers = async () => {
                 name: "superAdmin",
 				email: "auperadmin@superadmin.com",
                 password: bcrypt.hashSync('superAdmin123#', 5),
-                role: "super_admin"
+                role: "super_admin",
+				_id: new mongoose.Types.ObjectId("65ed7d2f6fa9305f1c42440d")
 			},
 			{
                 name: "admin",
 				email: "admin@admin.com",
 				password: bcrypt.hashSync('Admin1234#', 5),
-                role: "admin"
+                role: "admin",
+				_id: new mongoose.Types.ObjectId("65ed7d2f6fa9305f1c42440e")
 			},
             {
                 name: "user",
                 email: "user@user.com",
 				password: bcrypt.hashSync('User12345#', 5),
-                role: "user"
+                role: "user",
+				_id: new mongoose.Types.ObjectId("65ed7d2f6fa9305f1c42440f")
             },
             {
                 name: "user2",
                 email: "user2@user2.com",
 				password: bcrypt.hashSync('User12345#', 5),
-                role: "user"
+                role: "user",
+				_id: new mongoose.Types.ObjectId("65ed7d2f6fa9305f1c424410")
             }
 		]);
 		console.log("User created");
