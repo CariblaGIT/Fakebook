969ce2d4f2b47f43c7f814b37379cdf9c7172474
Caribla <caribla4@gmail.com>
2024-04-10 10:46:56 +0200

    Feat: Login => Setting into token the avatar and username to use on front

--- src/entities/auth/controller.js
+++ src/entities/auth/controller.js
@@ -68,6 +68,8 @@ export const loginUser = async (req, res) => {
         const token = jwt.sign(
             {
                 userId: user.id,
+                username: user.name,
+                avatar: user.avatar,
                 roleName: user.role
             },
             process.env.JWT_SECRET
